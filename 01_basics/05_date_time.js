bffc93a8ff70e4b7b4c6457f003a5d17bab105e2
Prajwal Vyavahare <122688935+Intex51@users.noreply.github.com>
2023-11-04 09:53:01 +0530

    Add files via upload
    This is basic in node js with the series code and chai by Hitesh Choudhary.to watch this codes use github profile: https://github.com/hiteshchoudhary/js-hindi-youtube .

---
+++ 01_basics/05_date_time.js
@@ -0,0 +1,21 @@
+
+let date = new Date()
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toDateString())
+// console.log(typeof date) 
+
+let myCreatedDate = new Date("1-14-2023")
+// console.log(myCreatedDate.toDateString()
+let myTime = Date.now()
+// console.log(myTime);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor((Date.now())/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
